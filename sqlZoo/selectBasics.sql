488105eb3755fa542924b27d00eaf807b9d8bcb9
girOly <girardob@gmail.com>
2019-10-08 15:58:27 -0400

    .sql refactor

--- sqlZoo/selectBasics.sql
+++ sqlZoo/selectBasics.sql
@@ -1,13 +1,13 @@
 <!-- https://sqlzoo.net/wiki/SELECT_basics -->
 
-## Ans 1
+-- ## Ans 1
 SELECT population FROM world
   WHERE name = 'Germany'
 
-## Ans 2
+-- ## Ans 2
 SELECT name, population FROM world
   WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
-## Ans 3
+-- ## Ans 3
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
